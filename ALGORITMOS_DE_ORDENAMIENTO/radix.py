8d3df4de30b0cb7ed65e714e4887b37e8a1f8ccf
Gustavo Carrillo <grafaelcarrillo@gmail.com>
2024-05-13 22:39:01 -0600

    Add files via upload

---
+++ ALGORITMOS_DE_ORDENAMIENTO/radix.py
@@ -0,0 +1,34 @@
+def radixsort(arr):
+    max1 = max(arr)
+    exp = 1
+    while max1 / exp >=1:
+        countingsort(arr, exp)
+        exp *=10
+
+def countingsort(arr, exp1):
+    n = len(arr)
+    output = [0] * (n)
+    count = [0] * (10)
+
+    for i in range(0, n):
+        index = arr[i] // exp1
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n -1
+    while i >= 0:
+        index = arr[i] // exp1
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    i = 0
+    for i in range(0, len(arr)):
+        arr[i] = output[i]
+
+arr = [4,6,2,5,8,9,5,10]
+print(arr)
+radixsort(arr)
+print(arr)
