0e020999c71db2bb663c5fa6a5a135eb3d513a1b
Gustavo Carrillo <grafaelcarrillo@gmail.com>
2024-05-22 16:50:10 -0600

    Update secuencial.py

--- ALGORITMOS_BÚSQUEDA/secuencial.py
+++ ALGORITMOS_BÚSQUEDA/secuencial.py
@@ -1,1 +1,13 @@
+def busquedaSecuencial(unaLista, item):
+	pos = 0
+	encontrado = False	
+	while pos < len(unaLista) and not encontrado:
+	    if unaLista[pos] == item:
+	        encontrado = True
+	    else:
+	        pos+=1
+	return encontrado
 
+listaPrueba = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+print(busquedaSecuencial(listaPrueba, 3))
+print(busquedaSecuencial(listaPrueba, 13))
